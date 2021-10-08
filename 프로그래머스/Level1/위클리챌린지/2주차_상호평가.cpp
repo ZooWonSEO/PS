b2d0d18cebc230d1a4f01196701dc0c36f1885f7
ZooWonSEO <seozoo123@gmail.com>
2021-10-08 13:37:31 +0900

    1008

--- 프로그래머스/Level1/위클리챌린지/2주차_상호평가.cpp
+++ 프로그래머스/Level1/위클리챌린지/2주차_상호평가.cpp
@@ -1,69 +1,54 @@
-#include <string>
-#include <vector>
-#include <iostream>
-using namespace std;
-
 /*
 풀이 : 조건을 잘봐야한다. 최소, 최댓값일 경우일지라도, 유일한 값이 아니면 배제하지 않는다.
 */
 
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 string solution(vector<vector<int>> scores) {
     string answer = "";
-    double std[11] = {0,};
+    int num = scores.size();
     
     for(int j=0; j<scores.size(); j++){
-        int mx = -1;
-        int mx_cnt = 0;
-        int mn = 101;
-        int mn_cnt = 0;
+        int total = 0;
+        int min_cnt = 0;
+        int max_cnt = 0;
         
         for(int i=0; i<scores[j].size(); i++){
-            if(scores[i][j]>=mx){
-                if(scores[i][j]==mx){
-                    mx_cnt++;
-                }
-                else{
-                    mx = scores[i][j];
-                    mx_cnt = 1;
-                }
+            if(scores[j][j]>scores[i][j]){
+                min_cnt++;
+            }    
+            if(scores[j][j]<scores[i][j]){
+                max_cnt++;
             }
-            if(scores[i][j]<=mn){
-                if(scores[i][j]==mn){
-                    mn_cnt++;
-                }
-                else{
-                    mn = scores[i][j];
-                    mn_cnt=1;
-                }
-            }
-            std[j] = std[j] + scores[i][j];
+            total = total + scores[i][j];
         }
         
-        int cnt = scores.size();
-        if((scores[j][j]==mx && mx_cnt==1) || (scores[j][j]==mn) && mn_cnt==1){
-            std[j] = std[j] - scores[j][j];
-            cnt--;
+        int avg = 0;
+        
+        if(min_cnt==(num-1) || max_cnt==(num-1)){
+            total = total - scores[j][j];
+            avg = total / (num-1);
         }
-        std[j] = std[j]/cnt;
-    }
-    
-    for(int i=0; i<scores.size(); i++){
+        else{
+            avg = total / num;
+        }
         
-        cout << std[i] << " ";
-        
-        if(std[i]>=90){
+        if(avg>=90){
             answer = answer + "A";
         }
-        else if(std[i]>=80 && std[i]<90){
+        else if(avg>=80 && avg<90){
             answer = answer + "B";
         }
-        else if(std[i]>=70 && std[i]<80){
+        else if(avg>=70 && avg<80){
             answer = answer + "C";
         }
-        else if(std[i]>=50 && std[i]<70){
+        else if(avg>=50 && avg<70){
             answer = answer + "D";
         }
-        else if(std[i]<50){
+        else if(avg<50){
             answer = answer + "F";
         }
     }
