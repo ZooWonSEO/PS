21f88c6539533b0281c0b3024b20539bda7b51bf
ZooWonSEO <seozoo123@gmail.com>
2021-10-08 18:24:15 +0900

    1008

--- 프로그래머스/Level1/위클리챌린지/4주차_직업군 추천하기.cpp
+++ 프로그래머스/Level1/위클리챌린지/4주차_직업군 추천하기.cpp
@@ -1,56 +1,99 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
 #include <map>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 string solution(vector<string> table, vector<string> languages, vector<int> preference) {
-    string answer = "";
-    string jobs[5] = {"SI", "CONTENTS", "HARDWARE", "PORTAL", "GAME"};
-    int score[5] = {0,};
-    map<string, int> m;
-    
-    for(int i=0; i<languages.size(); i++){
-        m[languages[i]] = preference[i];
-    }
-    
-    for(int i=0; i<table.size(); i++){
-        vector<string> v;
-        string tmp = "";
-        for(int j=0; j<table[i].size(); j++){
-            if(table[i][j]==' '){
-                v.push_back(tmp);
-                tmp = "";
-            }
-            else{
-                tmp = tmp + table[i][j];
-            }
-        }
-        v.push_back(tmp);
-        
-        for(int j=1; j<v.size(); j++){
-            if(m.find(v[j])!=m.end()){
-                score[i] = score[i] + (6-j)*m[v[j]];
-            }
-        }
-    }
-    
-    int mx = -1;
-    for(int i=0; i<5; i++){
-        if(score[i]>mx){
-            mx = score[i];
-        }
-    }
-    
-    vector<string> sv;
-    for(int i=0; i<5; i++){
-        if(score[i]==mx){
-            sv.push_back(jobs[i]);
-        }
-    }
-    
-    sort(sv.begin(), sv.end());
-    
-    return sv[0];
+
+	string jobs[5] = { "SI", "CONTENTS", "HARDWARE", "PORTAL", "GAME" };
+	map<string, int> m;
+	map<string, int>::iterator it;
+	int score[5] = { 0, };
+
+	for (int i = 0; i < preference.size(); i++) {
+		m[languages[i]] = preference[i];
+	}
+
+	for (int i = 0; i < table.size(); i++) {
+
+		string tmp = "";
+		vector<string> v;
+		
+		for (int j = 0; j < table[i].size(); j++) {
+			if (table[i][j] == ' ') {
+				v.push_back(tmp);
+				tmp = "";
+			}
+			else {
+				tmp = tmp + table[i][j];
+			}
+		}
+		v.push_back(tmp);
+
+		for (int j = 1; j < v.size(); j++) {
+			if (m.find(v[j]) != m.end()) {
+				score[i] = score[i] + (m[v[j]] * (6 - j));
+			}
+		}
+	}
+	
+	int mx = 0;
+	for (int i = 0; i < 5; i++) {
+		mx = max(mx, score[i]);
+	}	
+
+	vector<string> answer;
+	for (int i = 0; i < 5; i++) {
+		if (score[i] == mx) {
+			answer.push_back(jobs[i]);
+		}
+	}
+
+	sort(answer.begin(), answer.end());
+	
+	return answer[0];
+}
+
+int main(void)
+{
+
+	vector<string> table = { 
+		"SI JAVA JAVASCRIPT SQL PYTHON C#",
+		"CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+		"HARDWARE C C++ PYTHON JAVA JAVASCRIPT",
+		"PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+		"GAME C++ C# JAVASCRIPT C JAVA" };
+
+	/*
+	vector<string> languages = {
+		"PYTHON",
+		"C++",
+		"SQL"
+	};
+
+	vector<int> preference = {
+		7,
+		5,
+		5
+	};
+	*/
+	// HARDWARE
+
+
+	vector<string> languages = {
+		"JAVA",
+		"JAVASCRIPT"
+	};
+
+	vector<int> preference = {
+		7,
+		5
+	};
+	// PORTAL
+
+	cout << solution(table, languages, preference) << "\n";
+
+	return 0;
 }
