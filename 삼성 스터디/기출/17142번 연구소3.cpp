855aadae91b811d005ee7d6884a07dc735c8e207
ZooWonSEO <seozoo123@gmail.com>
2021-10-14 20:59:25 +0900

    1014

--- 삼성 스터디/기출/17142번 연구소3.cpp
+++ 삼성 스터디/기출/17142번 연구소3.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 int n, m;
 int map[52][52] = { 0, };
 
 vector<pair<int, int>> virus;
-int virus_visited[12] = { 0, };
-vector<int> active;
-
-const int dx[4] = {-1, 1, 0, 0};
-const int dy[4] = {0, 0, -1 ,1};
+vector<int> v;
+int v_visited[11] = { 0, };
 
 int tmp_map[52][52] = { 0, };
-int visited[52][52] = { -1, };
+int visited[52][52] = { 0, };
+
+const int dx[4] = {0, 0, -1, 1};
+const int dy[4] = {-1, 1, 0, 0};
 
 int emp = 0;
-int cnt = 0;
-
-int answer = 999999999;
+int answer = 999999;
 
 void init() {
-
-	for (int i = 0; i < 52; i++) {
-		for (int j = 0; j < 52; j++) {
-			tmp_map[i][j] = 0;
-			visited[i][j] = -1;
-		}
-	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			tmp_map[i][j] = map[i][j];
+			visited[i][j] = -1;
 		}
 	}
 
@@ -42,48 +34,48 @@ void init() {
 
 void bfs() {
 
-	init();
-
+	int cnt = 0;
 	int mx = 0;
-	cnt = 0;
+
+	init();
 
 	queue<pair<int, int>> q;
 
-	for (int i = 0; i < active.size(); i++) {
-		q.push(virus[active[i]]);
-		visited[virus[active[i]].first][virus[active[i]].second] = 0;
+	for (int i = 0; i < v.size(); i++) {
+		pair<int, int> p = virus[v[i]];
+		q.push(p);
+		visited[p.first][p.second] = 0;
 	}
 
 	while (!q.empty()) {
 
-		pair<int, int> p = q.front();
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
-
-		int x = p.first;
-		int y = p.second;
 
 		for (int i = 0; i < 4; i++) {
 
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if (nx < 0 || ny < 0 || nx >= n || ny >=n )
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n)
 				continue;
 
-			if (tmp_map[nx][ny] != 1 && visited[nx][ny] == -1) {
-				
+			if (visited[nx][ny] == -1 && tmp_map[nx][ny] != 1) {
+
 				visited[nx][ny] = visited[x][y] + 1;
+
 				if (tmp_map[nx][ny] == 0) {
 					cnt++;
 					mx = max(mx, visited[nx][ny]);
 				}
+
 				q.push(pair<int, int>(nx,ny));
 			}
-
 		}
 	}
-	
-	if (cnt == emp) {
+
+	if (emp == cnt) {
 		answer = min(answer, mx);
 	}
 
@@ -93,23 +85,21 @@ void bfs() {
 void dfs(int start, int cnt) {
 
 	if (cnt == m) {
-
 		bfs();
 		return;
 	}
 
 	for (int i = start; i < virus.size(); i++) {
 
-		if (virus_visited[i] == 1)
+		if (v_visited[i] == 1)
 			continue;
 
-		virus_visited[i] = 1;
-		active.push_back(i);
+		v_visited[i] = 1;
+		v.push_back(i);
 		dfs(i, cnt + 1);
-		active.pop_back();
-		virus_visited[i] = 0;
+		v.pop_back();
+		v_visited[i] = 0;
 	}
-
 	return;
 }
 
@@ -124,7 +114,7 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> map[i][j];
-			
+
 			if (map[i][j] == 2) {
 				virus.push_back(pair<int, int>(i,j));
 			}
@@ -137,10 +127,9 @@ int main() {
 
 	dfs(0, 0);
 
-	if (answer == 999999999) {
+	if (answer == 999999) {
 		answer = -1;
 	}
-
 	cout << answer;
 
 	return 0;
