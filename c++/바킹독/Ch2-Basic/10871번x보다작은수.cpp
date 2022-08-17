d70d0b3a55a6e4ed6dc31e5db3251359ff473be7
ZooWonSEO <seozoo123@gmail.com>
2022-08-17 21:34:30 +0900

    10871

--- c++/바킹독/Ch2-Basic/10871번x보다작은수.cpp
+++ c++/바킹독/Ch2-Basic/10871번x보다작은수.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 int main() {
 
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n, x;
 	cin >> n >> x;
 
 	for (int i = 0; i < n; i++) {
+
 		int input;
 		cin >> input;
-
+		
 		if (input < x) {
 			cout << input << " ";
 		}
 
 	}
-	
 
 	return 0;
 }
